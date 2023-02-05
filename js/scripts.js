fd3f605c9dfaa27e5eef29911c0ff9fcdb9a218b
Brandon Spear <Bowdowntotheepicturkey@gmail.com>
2023-02-04 21:14:35 -0800

    will update HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,75 @@
-// Business Logic
-  
+//Buisness Logic
+
 window.addEventListener("load", function() {
-  const form = document.getElementById("Test");
-  form.addEventListener("submit", function(event) { 
-    event.preventDefault();
-    console.log("hello")
-    const answer1 = document.querySelector("input[type=radio][name=Q1]:checked").value;
-    const answer2 = document.querySelector("input[type=radio][name=Q2]:checked").value;
-    const answer3 = document.querySelector("input[type=radio][name=Q3]:checked").value;
-    const answer4 = document.querySelector("input[type=radio][name=Q4]:checked").value;
-    const answer5 = document.querySelector("input[type=radio][name=Q5]:checked").value;
-    console.log(answer1, answer2, answer3, answer4, answer5)
+  let form = document.getElementById("formOne");
+  let resetBtn = document.getElementById("reset");
+  let outcome = document.getElementById("outcome");
+  let program = document.getElementById("program");
+  let cSharp = document.getElementById("cSharp");
+  let java = document.getElementById("java");
+  let ruby = document.getElementById("ruby");
+  let errormsg = document.getElementById("error-message");
+  console.log ("finished variables")
 
-// User Interface Logic
+form.addEventListener("submit", function(event) {
+  
+  
+  const nameInput = document.getElementById("nameInput").value;
+  const questionOne = document.getElementById("questionOne").value;
+  const questionTwo = parseInt(document.getElementById("questionTwo").value);
+  const questionThree = document.getElementById("questionThree").value; 
+  const questionFour = document.getElementById("questionFour").value;
+  const questionFive = parseInt(document.getElementById("questionFive").value);
 
-  let result;
-    if (answer1 === "1" && answer2 === "2" && answer3 === "3" && answer4 === "1" && answer5 === "2") {
-      result = "You should look into Python! Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis. Python is a general-purpose language, meaning it can be used to create a variety of different programs and isn't specialized for any specific problems.";
-    } else if (answer1 === "2" && answer2 === "3" && answer3 === "1" && answer4 === "2" && answer5 === "3") {
-      result = "You should look into C#! C# is a lightweight, interpreted, object-oriented language with first-class functions, and is best known as the scripting language for Web pages, but it's used in many non-browser environments as well.";
-    } else if (answer1 === "3" && answer2 === "1" && answer3 === "2" && answer4 === "3" && answer5 === "1") {
-      result = "You should look into C++! The programming language known as C++ (or “C-plus-plus”) is a general purpose coding language which has been used for game programming, software engineering, data structures, developing browsers, operating systems, applications, and more for over forty years.";  
-    } else {
-      result = "You should look into java script! Check out Epicodus for a great coding school! JavaScript (often shortened to JS) is a lightweight, interpreted, object-oriented language with first-class functions, and is best known as the scripting language for Web pages, but it's used in many non-browser environments as well."
+  document.querySelector("span#name1a").innerText = nameInput;
+  document.querySelector("span#question1a").innerText = questionOne;
+  document.querySelector("span#question2a").innerText = questionTwo;
+  document.querySelector("span#question3a").innerText = questionThree;
+  document.querySelector("span#question4a").innerText = questionFour;
+  document.querySelector("span#question5a").innerText = questionFive;
+
+  outcome.removeAttribute("class");
+
+  
+  
+  event.preventDefault();
+  console.log("finished function")
+  
+  if (questionTwo >= 11) {
+    errormsg.removeAttribute("class");
+    java.setAttribute("class", "hidden");
+    ruby.setAttribute("class", "hidden");
+    cSharp.setAttribute("class", "hidden");
   }
-console.log("hello");
-  document.getElementById("output").innerText = result;
+
+  if (questionTwo >= 7 && questionTwo <= 10) {
+    cSharp.removeAttribute("class");
+    java.setAttribute("class", "hidden");
+    ruby.setAttribute("class", "hidden");
+  } else if (questionTwo >= 4 && questionTwo <= 6) {
+    java.removeAttribute("class");
+    cSharp.setAttribute("class", "hidden");
+    ruby.setAttribute("class", "hidden");
+  } else if (questionTwo >= 1 && questionTwo <= 3) {
+    ruby.removeAttribute("class",)
+    cSharp.setAttribute("class", "hidden");
+    java.setAttribute("class", "hidden");
+  } 
+
+  });
+
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+  });
+
+  resetBtn.addEventListener("click", function() {
+    outcome.setAttribute("class", "hidden");
+    document.getElementById("nameInput").value = null;
+    document.getElementById("questionOne").value = null;
+    document.getElementById("questionTwo").value = null;
+    document.getElementById("questionThree").value = null;
+    document.getElementById("questionFour").value = null;
+    document.getElementById("questionFive").value = null;
+  
   });
 });
